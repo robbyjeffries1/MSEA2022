90843cf68aab8f9633021b750c8e79ee0d225b9b
robbyjeffries1 <robby.jeffries@att.net>
2021-09-04 12:05:30 -0500

    hw pt. 1

--- DataVisualization/DVHW/Assignment_1_blank.R
+++ DataVisualization/DVHW/Assignment_1_blank.R
@@ -5,6 +5,14 @@
 ## You'll need the packages 'tidyverse' and 'nycflights13'
 ## Hint: you need the install.packages() and library() commands
 
+## Un-comment the following two lines if you need to install the packages
+# install.packages("tidyverse")
+# install.packages("nycflights13")
+
+# import libraries 
+library(tidyverse)
+library(nycflights13)
+
 
 ## Note we will exclusively use the 'flights' dataset from 'nycflights13'
 #
@@ -13,12 +21,35 @@
 #
 ## (Hint: you're going to want multiple filters)
 
+flights %>%
+  filter(month==3, 
+         day==10, 
+         carrier=="DL", 
+         origin=="EWR")
 
+# There are 10 Delta flights that originated at Newark on 3/10/2013
 
-      ########[ YOUR CODE HERE]###########
 
 ## Step 3. Generate a histogram of the departure delays for each NYC airport.
 
+# Run summary to find the minimum and maximum dep_delay
+summary(flights$dep_delay == "NA")
+str(flights)
+
+ggplot(flights) + geom_histogram(aes(x = dep_delay, 
+                                     fill = origin), 
+                                 alpha = 0.5,
+                                 binwidth = 5)
+
+# EWR only
+flights %>%
+  ggplot(subset(flights$origin == "EWR"), aes(x = dep_delay)) + geom_histogram()
+
+
+
+delay_JFK <- flights %>% filter(origin == "JFK")
+
+delay_LGA <- flights %>% filter(origin == "LGA")
 
       ########[ YOUR CODE HERE]###########
 
@@ -27,6 +58,7 @@
 ## flight time (time in the air), grouped by the hour of the day it took off.
 ## Additionally show the average number of flights occuring at that hour every day.
 
+summary(flights)
      ########[ YOUR CODE HERE]###########
 
 
