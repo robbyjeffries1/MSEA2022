8a7e9caabbe477301874182283e9ad973575b514
robbyjeffries1 <robby.jeffries@att.net>
2021-09-04 10:37:24 -0500

    add DVHW folder

---
+++ DataVisualization/DVHW/Assignment_1_blank.R
@@ -0,0 +1,37 @@
+#### Please show all your code for each step
+
+
+## Step 1. Install/ load required libraries
+## You'll need the packages 'tidyverse' and 'nycflights13'
+## Hint: you need the install.packages() and library() commands
+
+
+## Note we will exclusively use the 'flights' dataset from 'nycflights13'
+#
+## Step 2. On March 10, 2013, how many Delta flights (carrier code "DL")
+## originated at Newark ("EWR")?  Show a dataframe/ tibble.
+#
+## (Hint: you're going to want multiple filters)
+
+
+
+      ########[ YOUR CODE HERE]###########
+
+## Step 3. Generate a histogram of the departure delays for each NYC airport.
+
+
+      ########[ YOUR CODE HERE]###########
+
+
+## Step 4. For flights from JFK ONLY, generate a tibble showing the average 
+## flight time (time in the air), grouped by the hour of the day it took off.
+## Additionally show the average number of flights occuring at that hour every day.
+
+     ########[ YOUR CODE HERE]###########
+
+
+## Step 5.  Using the df from above, generate a col chart with hour of the day on the x axis, and average
+## flight time on the y axis.
+## (Hint, you want to use geom_col())
+
+     ########[ YOUR CODE HERE]###########
