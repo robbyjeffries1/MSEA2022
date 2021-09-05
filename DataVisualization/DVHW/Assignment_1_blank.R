c474bd4d1408ea8f4f8068a1dc44fe7b9a9d5144
robbyjeffries1 <robby.jeffries@att.net>
2021-09-04 22:05:22 -0500

    sat evening work

--- DataVisualization/DVHW/Assignment_1_blank.R
+++ DataVisualization/DVHW/Assignment_1_blank.R
@@ -1,3 +1,8 @@
+#### Robby Jeffries
+#### 09-04-2021
+#### DVHW 1
+
+
 #### Please show all your code for each step
 
 
@@ -32,8 +37,10 @@ flights %>%
 
 ## Step 3. Generate a histogram of the departure delays for each NYC airport.
 
-# Run summary to find the minimum and maximum dep_delay
+# Run a summary to find the minimum and maximum departure delay times
+
 summary(flights$dep_delay == "NA")
+
 str(flights)
 
 ggplot(flights) + geom_histogram(aes(x = dep_delay, 
@@ -44,8 +51,6 @@ ggplot(flights) + geom_histogram(aes(x = dep_delay,
 # EWR only
 flights %>%
   ggplot(subset(flights$origin == "EWR"), aes(x = dep_delay)) + geom_histogram()
-
-
 
 delay_JFK <- flights %>% filter(origin == "JFK")
 
@@ -58,8 +63,22 @@ delay_LGA <- flights %>% filter(origin == "LGA")
 ## flight time (time in the air), grouped by the hour of the day it took off.
 ## Additionally show the average number of flights occuring at that hour every day.
 
+flights
+
+step4 <- flights %>% 
+  filter(origin=="JFK") %>%
+  group_by(hour) %>% 
+  summarize(avg_flight_time=mean(air_time, na.rm=TRUE), 
+            avg_number_of_flights=n()) # fix the avg_number_of_flights! right now, it's not showing the average. it's just a count :)
+
+step4
 summary(flights)
-     ########[ YOUR CODE HERE]###########
+summary(step4)
+flights %>%
+  group_by(hour, count=n()) %>% summarize(m = mean(count))
+
+sum(step4$avg_number_of_flights)
+
 
 
 ## Step 5.  Using the df from above, generate a col chart with hour of the day on the x axis, and average
