649d67f67be5775bf32f40fcf3849da93eab0f09
robbyjeffries1 <robby.jeffries@att.net>
2021-09-13 17:52:30 -0500

    project 1

---
+++ DataAnalytics/Project 1/project1.R
@@ -0,0 +1,14 @@
+### Data Analytics
+### Project 1
+### Written by: Robby Jeffries
+### 09-13-2021
+
+#@ If necessary, install the following packages
+# install.packages("udpipe")
+# install.packages("textrank")
+
+# Access libraries
+library(udpipe)
+library(textrank)
+
+
