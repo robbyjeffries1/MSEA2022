f7b90e3120b3c27c7c335fe46b64761b97eb676e
robbyjeffries1 <robby.jeffries@att.net>
2021-09-15 22:10:19 -0500

    project 1 edits

--- DataAnalytics/Project 1/project1.R
+++ DataAnalytics/Project 1/project1.R
@@ -7,11 +7,28 @@
 # install.packages("udpipe")
 # install.packages("textrank")
 
-# Access libraries
+# import libraries
 library(udpipe)
 library(textrank)
+library(dplyr)
+library(stringr)
 
+# store csv's as variables
 prc <- read.csv(file = "/Users/robbyjeffries/MSEA2022/DataAnalytics/Project 1/PRC_Data_Breach_Chronology_2020-01-13.csv")
+breach <- read.csv(file = "/Users/robbyjeffries/MSEA2022/DataAnalytics/Project 1/breach_report.csv")
+
+# count the number of occurrences
+email_count <- str_count(breach$Location.of.Breached.Information, "Email") %>% sum()
+network_count <- str_count(breach$Location.of.Breached.Information, "Network Server") %>% sum()
+paper_films_count <- str_count(breach$Location.of.Breached.Information, "Paper/Films") %>% sum()
+electronic_medical_record_count <- str_count(breach$Location.of.Breached.Information, "Electronic Medical Record") %>% sum()
+other_count <- str_count(breach$Location.of.Breached.Information, "Other") %>% sum()
+laptop_count <- str_count(breach$Location.of.Breached.Information, "Laptop") %>% sum()
+desktop_count <- str_count(breach$Location.of.Breached.Information, "Desktop Computer") %>% sum()
+
+# place all count variables in a single tibble
+count <- tibble(email_count, network_count, paper_films_count, electronic_medical_record_count, other_count, laptop_count, desktop_count)
+count
 
 # deleting punctuations
 prc$Description.of.incident <- gsub("[[:punct:][:blank:]]+", " ", prc$Description.of.incident)
