28a0c6c2ab919b83843799d90da9d8b3d84e47a7
robbyjeffries1 <robby.jeffries@att.net>
2021-10-12 13:30:55 -0500

    final edits

--- DataAnalytics/Project 1/project1.R
+++ DataAnalytics/Project 1/project1.R
@@ -19,17 +19,20 @@ other_count <- str_count(breach$Location.of.Breached.Information, "Other") %>% s
 laptop_count <- str_count(breach$Location.of.Breached.Information, "Laptop") %>% sum()
 desktop_count <- str_count(breach$Location.of.Breached.Information, "Desktop Computer") %>% sum()
 
-# How many times is "ransom" mentioned in the prc data?
-ransom_count <- str_count(prc$Description.of.incident, "ransomware") %>% sum()
-ransom_count
-
 # place all count variables in a single tibble
 count <- tibble(email_count, network_count, paper_films_count, electronic_medical_record_count, other_count, laptop_count, desktop_count)
 count
+
+# How many times is "ransom" mentioned in the prc data?
+ransom_count <- str_count(prc$Description.of.incident, "ransomware") %>% sum()
+ransom_count
 
 # Find the number of rows from Arkansas in each data set
 ar_count_prc <- str_count(prc$State, "Arkansas") %>% sum()
 ar_count_prc
 ar_count_breach <- str_count(breach$State, "AR") %>% sum()
 ar_count_breach
+
+
+breach %>% arrange(Individuals.Affected) %>% View()
 
